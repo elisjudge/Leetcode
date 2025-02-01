d42ea8716f6996c44c3e414cb2973f0daabdf222
elisjudge <101382345+elisjudge@users.noreply.github.com>
2025-02-01 18:58:35 +0900

    solved 102

---
+++ Python/102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,92 @@
+from typing import Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def construct_tree(values: list) -> TreeNode:
+    if not values:
+        return None
+    it = iter(values)
+    root = TreeNode(next(it))
+    nodes = [root]
+    for node in nodes:
+        val = next(it, None)
+        if val is not None:
+            node.left = TreeNode(val)
+            nodes.append(node.left)
+        val = next(it, None)
+        if val is not None:
+            node.right = TreeNode(val)
+            nodes.append(node.right)
+    return root
+
+def print_tree(tree, level=0, prefix="root"):
+    print(f"{level * '  '}{prefix:5s}: val={tree.val}")
+    if tree.left:
+        print_tree(tree.left, level + 1, "left")
+    if tree.right:
+        print_tree(tree.right, level + 1, "right")
+
+def convert_tree_to_list(tree: Optional[TreeNode], output=None, is_root=False):
+    if output is None:
+        output = []
+    if tree is None:
+        return output
+    if is_root:
+        output.append(tree.val)
+    if tree.left:
+        output.append(tree.left.val)
+    if tree.right:
+        output.append(tree.right.val)
+
+    if tree.left:
+        output = convert_tree_to_list(tree.left, output)
+    if tree.right:
+        output = convert_tree_to_list(tree.right, output)
+
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        if not root:
+            return []
+        
+        res = []
+        q = deque()
+        q.append(root)
+
+        while q:
+            q_length = len(q)
+            level = []
+            for _ in range(q_length):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        
+        return res
+
+s = Solution()
+
+testCases = [
+    {"root": [3,9,20,None,None,15,7], "expected": [[3],[9,20],[15,7]]},
+    {"root": [1], "expected": [[1]]},
+    {"root": [], "expected": []}
+]
+
+for i, testcase in enumerate(testCases):
+    root = construct_tree(testcase["root"])
+    output = s.levelOrder(root)
+    
+    if output == testcase["expected"]:
+        print(f"Test Case {i} Passed. Expected: {testcase['expected']}, Result: {output}")
+    else:
+        print(f"Test Case {i} Failed. Expected: {testcase['expected']}, Result: {output}")
