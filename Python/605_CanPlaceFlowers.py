031be8017918ef5a42018216066a8d6e3a235c33
elisjudge <101382345+elisjudge@users.noreply.github.com>
2025-02-14 11:30:28 +0900

    optimized 605

--- Python/605_CanPlaceFlowers.py
+++ Python/605_CanPlaceFlowers.py
@@ -1,51 +1,34 @@
 class Solution:
-    def __init__(self) -> None:
-        pass
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        prev, l, r = 0, 0, 1
-
-        if len(flowerbed)== 1:
-            if n == 0:
-                return True
-            elif n == 1 and flowerbed[l] == 0:
-                return True
-            else:
-                return False
-
-
-        while r < len(flowerbed):
-            if n == 0:
-                return True
-            
-            if prev == 0 and flowerbed[l] == 0 and flowerbed[r] == 0:
-                flowerbed[l] = 1
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f) - 1):
+            if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+                f[i] = 1
                 n -= 1
-            
-            prev = flowerbed[l]
-            l+=1
-            r+=1
-        
-        if prev == 0 and flowerbed[l] == 0:
-            flowerbed[l] = 1
-            n-= 1
-        
-        return n == 0
-     
-
-flowerbed1, n1 = [1,0,0,0,1], 1 # True
-flowerbed2, n2 = [1,0,0,0,1], 2 # False
-flowerbed3, n3 = [0], 0 # True
-flowerbed4, n4 = [1], 0 # True
-flowerbed5, n5 = [0], 1 # True
-flowerbed6, n6 = [1], 1 # False
+        return n <= 0
 
 s = Solution()
-print(s.canPlaceFlowers(flowerbed1, n1))
-print(s.canPlaceFlowers(flowerbed2, n2))
-print(s.canPlaceFlowers(flowerbed3, n3))
-print(s.canPlaceFlowers(flowerbed4, n4))
-print(s.canPlaceFlowers(flowerbed5, n5))
-print(s.canPlaceFlowers(flowerbed6, n6))
+
+testCases = [
+    {"flowerbed": [1,0,0,0,1], "n": 1, "expected": True},
+    {"flowerbed": [1,0,0,0,1], "n": 2, "expected": False},
+    {"flowerbed": [0,0,0], "n": 2, "expected": True},
+    {"flowerbed": [0,0,0], "n": 3, "expected": False},
+    {"flowerbed": [0,0,1], "n": 1, "expected": True},
+    {"flowerbed": [1,0,0], "n": 1, "expected": True},
+    {"flowerbed": [0,0,1], "n": 2, "expected": False},
+    {"flowerbed": [0,1], "n": 1, "expected": False},
+    {"flowerbed": [0,0], "n": 2, "expected": False},
+    {"flowerbed": [0,0], "n": 1, "expected": True},
+]
+
+for i, testcase in enumerate(testCases):
+    output = s.canPlaceFlowers(testcase["flowerbed"], testcase["n"])
+    
+    if output == testcase["expected"]:
+        print(f"Test Case {i} Passed. Expected: {testcase['expected']}, Result: {output}")
+    else:
+        print(f"Test Case {i} Failed. Expected: {testcase['expected']}, Result: {output}")
 
 
 
